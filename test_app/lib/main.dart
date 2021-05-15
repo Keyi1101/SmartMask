0c45c0efdd2f8c2e9ff20b524a86335fae95f7ab
weiqian-ni <77078947+weiqian-ni@users.noreply.github.com>
2021-05-15 12:34:35 +0800

    Update main.dart

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,113 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:aws_dynamodb_api/dynamodb-2012-08-10.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(MaterialApp(
+    title:'SmartMaskUI',
+    home:FirstScreen()
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    var stack=new Stack(
+      children:<Widget> [
+        new Positioned(
+          bottom:20.0,
+          right:10.0,
+          child:RouteButton()
+          ),
+        new Positioned(
+          bottom:0.0,
+          left:240.0,
+          child:new Text('Remaining Battery:')
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ]
     );
+
+    return Scaffold(
+      appBar:AppBar(title:Text("Your data")),
+      body:Center(
+        child: stack,
+      )
+    );
+  }
+}
+
+//跳转的Button
+class RouteButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed:(){
+          _navigateToSecondScreen(context);
+      },
+      child: Text('change your preference'),
+    );
+  }
+
+  _navigateToSecondScreen(BuildContext context) async{ //async是启用异步方法
+
+    final result = await Navigator.push(//等待
+      context, 
+      MaterialPageRoute(builder: (context)=> SecondScreen())
+      );
+
+      Scaffold.of(context).showSnackBar(SnackBar(content:Text('$result')));
+  }
+}
+
+class SecondScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+      appBar:AppBar(title:Text('change strategy of detection')),
+      body:Center(
+        child:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+      
+        children: <Widget>[
+        RaisedButton(
+          child:Text('High update frequency',style: TextStyle(fontSize: 20.0)),
+          onPressed: (){
+            Navigator.pop(context,'Mode set to: High update frequency');
+          },
+        ), 
+        RaisedButton(
+          child:Text('Medium update frequency',style: TextStyle(fontSize: 20.0)),
+          onPressed: (){
+            Navigator.pop(context,'Mode set to: Medium update frequency');
+          },
+        ),
+        RaisedButton(
+          child:Text('Low update frequency',style: TextStyle(fontSize: 20.0)),
+          onPressed: (){
+            Navigator.pop(context,'Mode set to: Low update frequency');
+          },
+        )
+        ]
+      )
+      )
+    );
+
   }
 }
