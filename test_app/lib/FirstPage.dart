f9763389b1fd81f9216a2488899afd3cdada09ab
weiqian-ni <77078947+weiqian-ni@users.noreply.github.com>
2021-05-18 14:52:51 +0800

    Update FirstPage.dart
    Add some pages to let the user see lagre detailed charts/tables.

--- test_app/lib/FirstPage.dart
+++ test_app/lib/FirstPage.dart
@@ -39,6 +39,13 @@ class _FirstScreen extends State<testNotificationScreen> {
               right:10.0,
               child:RouteButton()
           ),
+          
+          new Positioned(
+              bottom:10.0,
+              left:10.0,
+              child:RouteButtonToInterconnectedScreen()
+          ),
+
           new Positioned(
               bottom:80.0,
               right:30.0,
@@ -148,9 +155,9 @@ class RouteButton extends StatelessWidget {
     );
   }
 
-  _navigateToSecondScreen(BuildContext context) async{ //async是启用异步方法
+  _navigateToSecondScreen(BuildContext context) async{ 
 
-    final result = await Navigator.push(//等待
+    final result = await Navigator.push(
         context,
         MaterialPageRoute(builder: (context)=> SecondScreen())
     );
@@ -197,3 +204,185 @@ class SecondScreen extends StatelessWidget{
 
   }
 }
+
+
+
+class InterconnectedScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+        appBar:AppBar(title:Text('choose the details you want...')),
+        body:Center(
+            child:
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: <Widget>[
+                  Positioned(
+                    bottom:200.0,
+                    left:200.0,
+                    child:RouteButtonToStressAnalysis()
+                  ),
+                  Positioned(
+                    bottom:250.0,
+                    left:200.0,
+                    child:RouteButtonToHeartRateAnalysis()
+                  ),
+
+                  Positioned(
+                    bottom:300.0,
+                    left:200.0,
+                    child:RouteButtonToOverallAnalysis()
+                  ),
+                ]
+            )
+        )
+    );
+
+  }
+}
+
+
+
+
+class RouteButtonToStressAnalysis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed:(){
+        _navigateToStressAnalysis(context);
+      },
+      child: Text('Stress Analysis',style: TextStyle(fontSize: 20.0)),
+    );
+  }
+
+  _navigateToStressAnalysis(BuildContext context) async{ 
+
+    final result = await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> StressScreen())
+    );
+
+  }
+}
+
+
+class RouteButtonToHeartRateAnalysis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed:(){
+        _navigateToHeartRateAnalysis(context);
+      },
+      child: Text('Heart rate analysis',style: TextStyle(fontSize: 20.0)),
+    );
+  }
+
+  _navigateToHeartRateAnalysis(BuildContext context) async{ 
+
+    final result = await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> HeartRateScreen())
+    );
+
+  }
+}
+
+
+class RouteButtonToOverallAnalysis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed:(){
+        _navigateToOverallAnalysis(context);
+      },
+      child: Text('Overall analysis',style: TextStyle(fontSize: 20.0)),
+    );
+  }
+
+  _navigateToOverallAnalysis(BuildContext context) async{ 
+
+    final result = await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> OverallScreen())
+    );
+
+  }
+}
+
+
+class StressScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+        appBar:AppBar(title:Text('Your Stress Analysis')),
+        body:Center(
+            child:
+            Text('Chart of stress analysis is supposed to be placed here')
+        )
+    );
+
+  }
+}
+
+
+class HeartRateScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+        appBar:AppBar(title:Text('Your Heart Rate Analysis')),
+        body:Center(
+            child:
+            Text('Chart of heart rate analysis is supposed to be placed here')
+        )
+    );
+
+  }
+}
+
+
+
+class OverallScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+        appBar:AppBar(title:Text('Your overall Analysis')),
+        body:Center(
+            child:
+            Text('Chart of overall analysis is supposed to be placed here')
+        )
+    );
+
+  }
+}
+
+
+class RouteButtonToInterconnectedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed:(){
+        _navigateToInterConnectedScreen(context);
+      },
+      child: Text('See more details'), //this allows the user to see enlarged pictures for more details
+    );
+  }
+
+  _navigateToInterConnectedScreen(BuildContext context) async{ 
+
+    final result = await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> InterconnectedScreen())
+    );
+
+  }
+}
+
