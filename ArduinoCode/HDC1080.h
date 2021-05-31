f8bf0f5e73c70322d6ce30d263cfaa614ed9aa11
rickyqqj <78079207+rickyqqj@users.noreply.github.com>
2021-05-31 01:02:38 +0100

    Add files via upload

---
+++ ArduinoCode/HDC1080.h
@@ -0,0 +1,33 @@
+//This is the class for the tempurature/humidity sensor for the Mask.
+
+#pragma once
+
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+#define HDC1080_ADDR          0x40 //7-bit I2C Address
+#define HDC1080_CFG_REG       0x02 //config mode
+#define HDC1080_TEMP_REG      0x00 //read tempurature
+#define HDC1080_HUMI_REG      0x01 //read humidity
+
+
+#define HDC1080_RST 0x8000
+#define HDC1080_HEAT 0x2000
+#define HDC1080_TEMP_HUM 0x1000
+#define HDC1080_LOW_TRES 0x0400
+#define HDC1080_LOW_HRES 0x0200
+#define HDC1080_MID_HRES 0x0100
+
+class HDC1080 {
+ public: 
+  HDC1080(void);
+
+
+  private:
+ uint16_t CFG_REG_VAL;
+};
---
+++ ArduinoCode/central.ino
@@ -0,0 +1,699 @@
+
+
+/*
+  Optical SP02 Detection (SPK Algorithm) using the MAX30105 Breakout
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 19th, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
+
+  This demo shows heart rate and SPO2 levels.
+
+  It is best to attach the sensor to your finger using a rubber band or other tightening 
+  device. Humans are generally bad at applying constant pressure to a thing. When you 
+  press your finger against the sensor it varies enough to cause the blood in your 
+  finger to flow differently which causes the sensor readings to go wonky.
+
+  This example is based on MAXREFDES117 and RD117_LILYPAD.ino from Maxim. Their example
+  was modified to work with the SparkFun MAX30105 library and to compile under Arduino 1.6.11
+  Please see license file for more info.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+ 
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"//by spark fun, free 
+//#include "algorithm_by_RF.h"//by aromring on Github, free
+
+#include "heartRate.h"
+
+#include "calendar.h" //software RTC
+#include <EnergySaving.h>
+#include <TimerTCC0.h>
+
+
+//you need to download the following libraries.
+#include "RTCZero.h" //this is the build_in HW real time clock, but it do not have a time stamp
+//#include <ArduinoLowPower.h>
+/*
+#include <EnergySaving.h>
+//C:\Users\hp\AppData\Local\Arduino15\packages\Seeeduino\hardware\samd\1.8.1\libraries\EnergySaving\src
+*/
+
+
+MAX30105 particleSensor;
+
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+bool spoi_done=0;
+
+float beatsPerMinute;
+int beatAvg;
+bool No_spoi=0;//1 to disable spoi algorithmn to speed up 
+
+
+#define MAX_NAME_LENGTH 20
+char NAME_OF_MASK[MAX_NAME_LENGTH]="Smart Mask";
+
+#define MASK_COMMAND_SIZE 10
+#define BLE_STAT_PIN 2
+#define MAX_BRIGHTNESS 255
+#define SLEEP_TIME_uS 1000000
+#define IN_DEBUGING 1 //1 to prevent the seeduino from sleeping
+
+
+#define Data_Set_Length_Max 2000
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+uint32_t irCurrent;
+uint32_t redCurrent;
+uint8_t spoiRead=0;
+uint8_t spoiPTR=0;
+
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+//float spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+float signalRatio;
+float signalCorel;
+
+byte pulseLED = 9; //Must be on PWM pin
+byte readLED = 10; //Blinks with each data read
+
+uint8_t NextSecond=0; //the alarm will take place when time change to the next second.
+
+
+struct Data_Set{
+  uint32_t Second_Stamp_S;
+  uint8_t Heart_Rate_S;
+  uint8_t SPO2_S;
+
+  
+};
+
+
+struct PPG_Set{
+uint32_t Milli_Stamp;
+uint32_t PPG_Red;
+uint32_t PPG_IR;
+};
+uint16_t BLE_Data_Read_RC=0;
+uint16_t BLE_Data_Write_RC=0;
+
+#define PPG_List_Length 2000
+struct PPG_List{
+ uint32_t Second_Stamp; //start
+ PPG_Set PPG_Sig[PPG_List_Length];
+};
+bool BLE_LAST_STAT=0;
+Data_Set Normal_Mode_Data[Data_Set_Length_Max];
+
+datetime My_Time;
+RTCZero rtc;
+EnergySaving nrgSave;
+
+bool isLEDOn = false;
+bool Mask_On = false; //detect whether the user is wearing the mask
+
+void setup()
+{
+   pinMode(13, OUTPUT); 
+  
+  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+  Serial1.begin(9600);
+  Serial.println(F("setting up"));
+  pinMode(pulseLED, OUTPUT);
+  pinMode(readLED, OUTPUT);
+ pinMode(BLE_STAT_PIN,INPUT);
+  pinMode(1, INPUT);
+  attachInterrupt(1, NewPPG,FALLING);
+
+  
+
+
+
+  
+  My_Time.init_Datetime(); //start the soft RTC
+  
+  
+  rtc.begin(); //start the hard RTC
+  rtc.setTime(0,0,0);
+  rtc.setDate(0,1,1);//Year offset? 
+  rtc.enableAlarm(rtc.MATCH_SS);//second matched alarm
+  rtc.setAlarmSeconds(NextSecond);
+  rtc.attachInterrupt(RTC_Isr);
+  nrgSave.begin(WAKE_RTC_ALARM);  //standby setup for external interrupts
+  //rtc.setAlarm();
+
+  
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
+
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0) ; //wait until user presses a key
+  Serial.read();
+
+  Serial1.print("AT+STARTEN1\r\n");
+  delay(100);
+  Serial1.print("AT+NAME");
+  Serial1.print(NAME_OF_MASK);//not work
+  Serial1.print("\r\n");
+  delay(100);
+  Serial1.print("AT+RST\r\n");
+
+
+
+ Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+   while (Serial.available() == 0) ; //wait until user presses a key
+  Serial.read();
+
+  
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 2; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+ 
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  //particleSensor.setup();
+ //  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
+ //    TimerTcc0.initialize((100000/sampleRate*sampleAverage));
+
+
+}
+
+void loop()
+{ 
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+
+  My_Time.To_Stamp(2021, 5, 23, 9, 16, 0);
+  Serial.println(My_Time.Get_Stamp());
+
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+ //   while (particleSensor.available() == false) //do we have new data?
+ //     particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+  
+    irBuffer[i] = particleSensor.getIR();
+ 
+    
+ //   particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+   // rf_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate, &signalRatio, &signalCorel);
+      for (byte i = 25; i < 100; i++)
+      {
+       redBuffer[i - 25] = redBuffer[i];
+       irBuffer[i - 25] = irBuffer[i];
+      }
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+ // TimerTcc0.attachInterrupt(timerIsr);//not working
+  char MASK_COMMAND[MASK_COMMAND_SIZE];
+  uint8_t COMMAND_COUNT=0;
+  while (1)
+  {
+
+      if(Serial1.available())
+      {COMMAND_COUNT=0;
+       Serial.printf("Bluetooth:");
+        while(Serial1.available() && COMMAND_COUNT<MASK_COMMAND_SIZE)
+       { 
+        MASK_COMMAND[COMMAND_COUNT]=Serial1.read();
+        Serial.print(MASK_COMMAND[COMMAND_COUNT]);
+        COMMAND_COUNT++;
+       }
+      
+      }
+   if(Mask_On)
+    {
+    
+      Mask_On=0;
+ //    particleSensor.wakeUp(); 
+    
+     
+  //    while (particleSensor.available() == false) //do we have new data?
+  //      particleSensor.check(); //Check the sensor for new data
+  //    digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+   //do not get red here to save time.
+   
+      irCurrent = particleSensor.getIR();
+
+    spoiRead++;
+
+  
+   if(spoiRead==4 && No_spoi==0 )
+   {spoiRead=0;
+
+
+    redBuffer[spoiPTR+75] =  particleSensor.getRed();
+    irBuffer[spoiPTR+75] =  irCurrent;
+    spoiPTR++;
+    
+   if(spoiPTR==25)
+    {spoiPTR=0;
+      
+      maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+      //rf_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate, &signalRatio, &signalCorel);
+      for (byte i = 25; i < 100; i++)
+      {
+       redBuffer[i - 25] = redBuffer[i];
+       irBuffer[i - 25] = irBuffer[i];
+       }
+
+            //send samples and calculation result to terminal program through UART
+         if(digitalRead(BLE_STAT_PIN)) //BLE connected, send the data
+         {if(BLE_LAST_STAT == digitalRead(BLE_STAT_PIN))
+          {while (BLE_Data_Read_RC!=BLE_Data_Write_RC)
+           {
+          Serial1.print("{");
+          Serial1.print('"');
+          Serial1.print("Stamp");
+          Serial1.print('"');
+          Serial1.print(':');
+          Serial1.print(Normal_Mode_Data[BLE_Data_Read_RC].Second_Stamp_S, DEC);
+          Serial1.print(',');
+
+          
+          Serial1.print('"');
+          Serial1.print("SPO2");
+          Serial1.print('"');
+          Serial1.print(':');
+          Serial1.print(Normal_Mode_Data[BLE_Data_Read_RC].SPO2_S, DEC);
+          Serial1.print(',');
+
+         
+          Serial1.print('"');
+          Serial1.print("Heart_Rate");
+          Serial1.print('"');
+          Serial1.print(':');
+          Serial1.print(Normal_Mode_Data[BLE_Data_Read_RC].Heart_Rate_S, DEC);
+          Serial1.print(',');
+
+
+
+          Serial1.print('}');
+          Serial1.print("\r\n");
+          BLE_Data_Read_RC++;
+           if(BLE_Data_Read_RC>=Data_Set_Length_Max)
+            {
+            BLE_Data_Read_RC=0;
+            }  
+
+          
+          }
+          }
+         }
+   
+           
+
+           
+           Normal_Mode_Data[BLE_Data_Write_RC].Heart_Rate_S=beatAvg;
+           Normal_Mode_Data[BLE_Data_Write_RC].Second_Stamp_S=My_Time.Get_Stamp();
+           Normal_Mode_Data[BLE_Data_Write_RC].SPO2_S=spo2;
+
+
+           BLE_Data_Write_RC++;
+           if(BLE_Data_Write_RC>=Data_Set_Length_Max)
+           {
+            BLE_Data_Write_RC=0;
+           }
+           if(BLE_Data_Write_RC==BLE_Data_Read_RC)
+           {
+            BLE_Data_Read_RC++;
+            if(BLE_Data_Read_RC>=Data_Set_Length_Max)
+            {
+            BLE_Data_Read_RC=0;
+            }  
+           }
+           BLE_LAST_STAT=digitalRead(BLE_STAT_PIN);
+          
+         
+ /*    Serial.print(My_Time.Get_Hour());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Minute());
+     Serial.print(" "); 
+     Serial.println(My_Time.Get_Second());
+      Serial.print(" "); */
+
+    /*
+      Serial.print(F("red="));
+      Serial.print(redBuffer[99], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[99], DEC);
+*/
+      Serial.print(F(", HRspoi="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(", HRPBA=");
+      Serial.print(beatsPerMinute);
+      Serial.print(", Avg BPM=");
+      Serial.print(beatAvg);
+
+   
+   /*   Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);*/
+
+
+
+      Serial.print(F(", SPO2="));
+      Serial.println(spo2, DEC);
+
+  /*    Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);*/
+    
+    
+    
+     }
+   } 
+      
+
+       Mask_On=Mask_On || (irCurrent>50000);
+
+   //     particleSensor.nextSample(); //We're finished with this sample so move to next sample
+  
+      
+
+  if (checkForBeat((long)(irCurrent)) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 200 && beatsPerMinute > 35)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  } 
+/*
+      Serial.print(", HRPBA=");
+      Serial.print(beatsPerMinute);
+      Serial.print(", Avg BPM=");
+      Serial.println(beatAvg);
+*/
+    
+    
+     
+    //PPG test
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+  
+    //take 25 sets of samples before calculating the heart rate.
+
+   /* 
+    for (byte i = 75; i < 100; i++)
+    { */
+     
+     
+ 
+
+  //  }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    
+  
+ 
+  
+
+   /*
+
+   //calendar test
+   if(My_Time.To_Stamp(My_Time.Get_Year(), My_Time.Get_Month(), My_Time.Get_Day(), My_Time.Get_Hour(), My_Time.Get_Minute(), My_Time.Get_Second()))
+   {
+    Serial.read();
+   }
+   
+   My_Time.To_Datetime(My_Time.Get_Stamp());
+   
+     Serial.print(My_Time.Get_Stamp());
+    Serial.print(My_Time.Get_Year());
+     Serial.print(" ");
+      Serial.print(My_Time.Get_Month());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Day());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Hour());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Minute());
+     Serial.print(" "); 
+     Serial.println(My_Time.Get_Second());
+     for(int i=0; i<3600; i++)
+     {My_Time.Next_Datetime();}
+   */
+   
+  }
+  if(!Mask_On)
+   {Mask_On=0;
+    particleSensor.clearFIFO();
+    particleSensor.shutDown(); 
+
+    if(!IN_DEBUGING)
+    {nrgSave.standby();}  //now mcu goes in standby mode, this will affect debugging
+    else
+    {delay(1000);} //fake sleep
+    
+      particleSensor.wakeUp(); 
+         if(digitalRead(BLE_STAT_PIN)) //BLE connected, send the data
+         {if(BLE_LAST_STAT == digitalRead(BLE_STAT_PIN))
+          {while (BLE_Data_Read_RC!=BLE_Data_Write_RC)
+           {
+          Serial1.print("{");
+          Serial1.print('"');
+          Serial1.print("Stamp");
+          Serial1.print('"');
+          Serial1.print(':');
+          Serial1.print(Normal_Mode_Data[BLE_Data_Read_RC].Second_Stamp_S, DEC);
+          Serial1.print(',');
+
+          
+          Serial1.print('"');
+          Serial1.print("SPO2");
+          Serial1.print('"');
+          Serial1.print(':');
+          Serial1.print(Normal_Mode_Data[BLE_Data_Read_RC].SPO2_S, DEC);
+          Serial1.print(',');
+
+         
+          Serial1.print('"');
+          Serial1.print("Heart_Rate");
+          Serial1.print('"');
+          Serial1.print(':');
+          Serial1.print(Normal_Mode_Data[BLE_Data_Read_RC].Heart_Rate_S, DEC);
+          Serial1.print(',');
+
+
+
+          Serial1.print('}');
+          Serial1.print("\r\n");
+          BLE_Data_Read_RC++;
+           if(BLE_Data_Read_RC>=Data_Set_Length_Max)
+            {
+            BLE_Data_Read_RC=0;
+            }  
+
+          
+          }
+          }
+         }
+         BLE_LAST_STAT = digitalRead(BLE_STAT_PIN);
+      
+ //   while (particleSensor.available() == false) //do we have new data?
+ //    particleSensor.check(); //Check the sensor for new data
+    //  redBuffer[0] = particleSensor.getRed();
+      irBuffer[0] = particleSensor.getIR();
+  //     particleSensor.nextSample(); //We're finished with this sample so move to next sample
+     if((irBuffer[0]>50000))
+     {Mask_On=1;
+      
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+ //   while (particleSensor.available() == false) //do we have new data?
+ //     particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+  
+    irBuffer[i] = particleSensor.getIR();
+ 
+    
+ //   particleSensor.nextSample(); //We're finished with this sample so move to next sample
+  /*
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC); */
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  // rf_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate, &signalRatio, &signalCorel);
+     
+      for (byte i = 25; i < 100; i++)
+      {
+       redBuffer[i - 25] = redBuffer[i];
+       irBuffer[i - 25] = irBuffer[i];
+      }
+      
+      
+     }
+   }
+  
+
+    
+
+  
+
+  // LowPower.sleep(1);  //now mcu goes in standby mode
+  }
+}
+
+void timerIsr() // not work
+{if(Mask_On)
+    {Mask_On=0;
+ //    particleSensor.wakeUp(); 
+    
+     
+  //    while (particleSensor.available() == false) //do we have new data?
+  //      particleSensor.check(); //Check the sensor for new data
+  //    digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redCurrent = particleSensor.getRed();
+      irCurrent = particleSensor.getIR();
+
+       Mask_On=Mask_On || (irCurrent>50000);
+
+   //     particleSensor.nextSample(); //We're finished with this sample so move to next sample
+  
+      
+
+  
+     
+    //PPG test
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+  
+    //take 25 sets of samples before calculating the heart rate.
+
+   /* 
+    for (byte i = 75; i < 100; i++)
+    { */
+     
+     
+ 
+
+  //  }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    
+    spoiRead++;
+    spoi_done=0;
+
+  
+   if(spoiRead==6)
+   {spoiRead=0;
+    spoi_done=0;
+
+    
+   }
+
+   /*
+
+   //calendar test
+   if(My_Time.To_Stamp(My_Time.Get_Year(), My_Time.Get_Month(), My_Time.Get_Day(), My_Time.Get_Hour(), My_Time.Get_Minute(), My_Time.Get_Second()))
+   {
+    Serial.read();
+   }
+   
+   My_Time.To_Datetime(My_Time.Get_Stamp());
+   
+     Serial.print(My_Time.Get_Stamp());
+    Serial.print(My_Time.Get_Year());
+     Serial.print(" ");
+      Serial.print(My_Time.Get_Month());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Day());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Hour());
+     Serial.print(" "); 
+     Serial.print(My_Time.Get_Minute());
+     Serial.print(" "); 
+     Serial.println(My_Time.Get_Second());
+     for(int i=0; i<3600; i++)
+     {My_Time.Next_Datetime();}
+   */
+   
+  }
+  
+
+ 
+
+  
+}
+void NewPPG()//not used
+{  digitalWrite(13, isLEDOn);
+   isLEDOn = !isLEDOn;
+  
+}
+
+
+void RTC_Isr()  //the MCU is waked up by RTC,every 1 second
+{ My_Time.Next_Datetime();//run the soft RTC
+ // My_Time.Next_Datetime();//run the soft RTC
+  NextSecond=rtc.getSeconds();//set next wake up
+  
+  rtc.setAlarmSeconds(NextSecond);
+ // digitalWrite(13, isLEDOn);
+ // isLEDOn = !isLEDOn;
+}
