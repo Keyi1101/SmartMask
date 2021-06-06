c4e0f22f6dfe4d432728a4c06f60ccb62f4e21a2
weiqian-ni <77078947+weiqian-ni@users.noreply.github.com>
2021-06-06 14:35:47 +0800

    Update main.dart

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'state.dart';
 import 'model/create_item.dart';
 import 'package:test_app/model/product.dart';
-//import 'FirstPage.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 
 void main() {
@@ -14,13 +14,15 @@ class MyApp extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
-    Product product = new Product(heartrate:"80", temperature:"37",movement: "run", oxygenconc:"90",);
-    createItem(product);
+    //Product product = new Product(heartrate:"120", temperature:"37.2",movement: "walk", oxygenconc:"95%",);
+    
+    //create_the_stuff('"createdAt":3266,"heartrate":85,"movement":active,"oxygenconc.":98,"temperature":37');
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Mask',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: testNotificationScreen(),
     );
   }
