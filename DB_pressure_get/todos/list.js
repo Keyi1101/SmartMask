f7d4893cf51e3b544b7ec98670efbddddb01de6f
keyi1101 <keyiwu1101@gamil.com>
2021-06-23 17:04:57 +0800

    update node.js
    two more node.js connecting with other two DynamoDB

---
+++ DB_pressure_get/todos/list.js
@@ -0,0 +1,28 @@
+'use strict';
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: "stress",
+};
+
+module.exports.list = (event, context, callback) => {
+  // fetch all todos from the database
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todos.',
+      });
+      return;
+    }
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
---
+++ DB_product/todos/create.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  
+  if (typeof data.tim !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the todo item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: "Products",
+    Item: {
+      ID:uuid.v1(),
+      time:data.tim,
+      tresp:data.tresp,
+      tskin:data.tskin,
+      tenv:data.tenv,
+      accelx:data.accelx,
+      accely:data.accely,
+      accelz:data.accelz,
+      ppgir:data.ppgir,
+      ppgred:data.ppgred
+    },
+  };
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
